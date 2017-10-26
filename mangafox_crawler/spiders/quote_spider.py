ca37fb8403ba48791fe574611973da75c04ecf7c
dipu-bd <dipu.sudipta@gmail.com>
2017-10-26 10:34:59 +0600

    Add author scrapper

--- mangafox_crawler/spiders/quote_spider.py
+++ mangafox_crawler/spiders/quote_spider.py
@@ -1,23 +1,42 @@
-import os
 import scrapy
 
 
 class QuotesSpider(scrapy.Spider):
     name = "quotes"
-
-    def start_requests(self):
-        urls = [
-            'http://quotes.toscrape.com/page/1/',
-            'http://quotes.toscrape.com/page/2/',
-        ]
-        for url in urls:
-            yield scrapy.Request(url=url, callback=self.parse)
+    start_urls = [
+        'http://quotes.toscrape.com/page/1/',
+        'http://quotes.toscrape.com/page/2/',
+    ]
 
     def parse(self, response):
-        page = response.url.split("/")[-2]
-        if not os.path.exists('downloads'):
-            os.mkdir('downloads')
-        filename = os.path.join('downloads', 'quotes-%s.html' % page)
-        with open(filename, 'wb') as f:
-            f.write(response.body)
-        self.log('Saved file %s' % filename)
+        for quote in response.css('div.quote'):
+            yield {
+                'text': quote.css('span.text::text').extract_first(),
+                'author': quote.css('small.author::text').extract_first(),
+                'tags': quote.css('div.tags a.tag::text').extract(),
+            }
+
+
+class AuthorSpider(scrapy.Spider):
+    name = 'authors'
+
+    start_urls = ['http://quotes.toscrape.com/']
+
+    def parse(self, response):
+        # follow links to author pages
+        for href in response.css('.author + a::attr(href)'):
+            yield response.follow(href, self.parse_author)
+
+        # follow pagination links
+        for href in response.css('li.next a::attr(href)'):
+            yield response.follow(href, self.parse)
+
+    def parse_author(self, response):
+        def extract_with_css(query):
+            return response.css(query).extract_first().strip()
+
+        yield {
+            'name': extract_with_css('h3.author-title::text'),
+            'birthdate': extract_with_css('.author-born-date::text'),
+            'bio': extract_with_css('.author-description::text'),
+        }
