a2412bd5895a8369f2842f9cff676c1725d8b27b
dipu-bd <dipu.sudipta@gmail.com>
2017-10-26 14:12:42 +0600

    Grab details info

--- mangafox_crawler/spiders/mangafox_spider.py
+++ mangafox_crawler/spiders/mangafox_spider.py
@@ -1,7 +1,14 @@
 # -*- coding: utf-8 -*-
+import logging
+import json
+import requests
 import scrapy
 
+
 class MangafoxSpider(scrapy.Spider):
+    """
+    Crawler to grab list of Manga from MangaFox
+    """
     name = 'mangafox'
 
     start_urls = ['https://mangafox.me/manga/']
@@ -9,18 +16,40 @@ class MangafoxSpider(scrapy.Spider):
     def parse(self, response):
         selector = 'div.manga_list ul li a'
         for item in response.css(selector):
-            link = item.css('::attr(href)').extract_first()
+            item = response.css(selector)[0]
+            sid = item.css('::attr(rel)').extract_first()
             yield {
-                '_id': item.css('::attr(rel)').extract_first(),
+                'sid': sid,
                 'title': item.css('::text').extract_first(),
+                'link': response.urljoin(item.css('::attr(href)').extract_first()),
                 'completed': len(item.css('.manga_close')) == 1,
-                'details': response.follow(link, self.parse),
+                'details': self.get_details(sid)
             }
+            logging.debug('Crawled ' + sid)
         # end for
     # end def
 
-    def parse_details(self, response):
-        return response.url
+    def get_details(self, sid):
+        """
+        Get details information about the manga from the sid
+        """
+        payload = {'sid': sid}
+        url = 'http://mangafox.me/ajax/series.php'
+        response = requests.post(url, data=payload)
+        result = json.loads(response.text)
+        return {
+            'display_name': result[0],
+            'alternate_names': [s.strip() for s in result[1].split(';')],
+            'genres': [s.strip() for s in result[2].split(',')],
+            'author': result[3],
+            'artist': result[4],
+            'rank': ''.join([x for x in result[5] if x.isdigit()]),
+            'stars': result[6],
+            'rating': result[7],
+            'year': result[8],
+            'description': result[9],
+            'cover': result[10]
+        }
     # end def
 
 # end class
