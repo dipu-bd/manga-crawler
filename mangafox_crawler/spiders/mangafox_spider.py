74e65527b0c4fd04cfb1d7a65d7964f5b35da2de
dipu-bd <dipu.sudipta@gmail.com>
2017-10-26 12:16:39 +0600

    mangafox: Add new method: parse_details

--- mangafox_crawler/spiders/mangafox_spider.py
+++ mangafox_crawler/spiders/mangafox_spider.py
@@ -9,13 +9,18 @@ class MangafoxSpider(scrapy.Spider):
     def parse(self, response):
         selector = 'div.manga_list ul li a'
         for item in response.css(selector):
+            link = item.css('::attr(href)').extract_first()
             yield {
                 '_id': item.css('::attr(rel)').extract_first(),
                 'title': item.css('::text').extract_first(),
                 'completed': len(item.css('.manga_close')) == 1,
-                'link': item.css('::attr(href)').extract_first(),
+                'details': response.follow(link, self.parse),
             }
         # end for
+    # end def
+
+    def parse_details(self, response):
+        return response.url
     # end def
 
 # end class
--- mangafox_crawler/spiders/quote_spider.py
+++ mangafox_crawler/spiders/quote_spider.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 import scrapy
 
 
@@ -40,3 +41,22 @@ class AuthorSpider(scrapy.Spider):
             'birthdate': extract_with_css('.author-born-date::text'),
             'bio': extract_with_css('.author-description::text'),
         }
+
+
+class ToScrapeCSSSpider(scrapy.Spider):
+    name = "toscrape-css"
+    start_urls = [
+        'http://quotes.toscrape.com/',
+    ]
+
+    def parse(self, response):
+        for quote in response.css("div.quote"):
+            yield {
+                'text': quote.css("span.text::text").extract_first(),
+                'author': quote.css("small.author::text").extract_first(),
+                'tags': quote.css("div.tags > a.tag::text").extract()
+            }
+
+        next_page_url = response.css("li.next > a::attr(href)").extract_first()
+        if next_page_url is not None:
+            yield scrapy.Request(response.urljoin(next_page_url))
