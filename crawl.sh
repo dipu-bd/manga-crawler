473135e6812f0c1512a54295bb42791e3340e0fa
dipu-bd <dipu.sudipta@gmail.com>
2017-10-27 16:47:22 +0600

    Backup and remove old logs

--- crawl.sh
+++ crawl.sh
@@ -1,29 +1,40 @@
+#!/usr/bin/env bash
+
+# Backup old log
+logfile="logs/scrapy.log"
+if [ -f $logfile ]; then
+   mv -f $logfile $logfile".old"
+fi
+mkdir -p "$(dirname "$logfile")" || exit
+touch "$logfile"
+
+cat <<EOT >> $logfile
+================================================================================
+Crawling Manga Genres from MangaFox
+================================================================================
+EOT
+
 start=`date +%s`
 
-rm -f scrapy.log
-
-cat <<EOT >> scrapy.log
-================================================================================
-Crawling Genres from MangaFox
-================================================================================
-EOT
-
-scrapy crawl mangafox_genres -L INFO
-
-cat <<EOT >> scrapy.log
-
-
-================================================================================
-Crawling Index from MangaFox
-================================================================================
-EOT
-
-scrapy crawl mangafox_index -L INFO
+scrapy crawl mangafox_genres -L INFO --logfile="$logfile"
 
 end=`date +%s`
 runtime=$((end-start))
 
-cat <<EOT >> scrapy.log
+cat <<EOT >> $logfile
+
+
+================================================================================
+Crawling Manga Index from MangaFox
+================================================================================
+EOT
+
+scrapy crawl mangafox_index -L INFO --logfile="$logfile"
+
+end=`date +%s`
+runtime=$((runtime+end-start))
+
+cat <<EOT >> $logfile
 
 ================================================================================
 Runtime: $runtime seconds.
