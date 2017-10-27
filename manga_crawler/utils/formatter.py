92f4c17053c9bc2ab5cb3ec80ea60636d53ad636
dipu-bd <dipu.sudipta@gmail.com>
2017-10-27 16:47:05 +0600

    Add uitls.formatter functions

---
+++ manga_crawler/utils/formatter.py
@@ -0,0 +1,32 @@
+# -*- conding: utf-8 -*-
+import json
+import bleach
+from urllib.parse import parse_qs
+
+def parseJson(val):
+    return json.loads(val)
+# end def
+
+def parseQuery(val):
+    return parse_qs(val.decode("utf-8"))
+# end def
+
+def parseInt(val):
+    return int('0' + cleanStr(val))
+# end def
+
+def parseFloat(val):
+    return float('0' + cleanStr(val))
+# end def
+
+def parseOrdinal(val):
+    return parseInt(''.join(filter(str.isdigit, val)))
+# end def
+
+def cleanStr(val):
+    return bleach.clean(str(val), tags=[], strip=True).strip()
+# end def
+
+def splitStr(val, sep):
+    return [s.strip() for s in cleanStr(val).split(sep)]
+# end def
